f990bb6b833fc699dd1a0bfd5b1b18008054f1b6
Ashika.S.Athavar <36147282+ashikasathavar@users.noreply.github.com>
2018-04-16 11:33:19 +0530

    Create final.php

---
+++ final.php
@@ -0,0 +1,19 @@
+<html>
+<head><title>final page</title>
+	<style>
+		body
+		{
+		background-color:lightblue;
+		}
+	</style>
+</head>
+	<body>
+		<?php
+		echo "<center>";
+		echo "<br />";
+		echo "<h2>"."You have casted your vote sucessfully"."</h2>";
+		echo " '<a href="logout.php">'.LOGOUT.'</a>'";
+		echo "</center>";
+		?>
+	</body>
+</html>
